8a47e3a9cdd9c839fcc40eaaf39a73d3a7f9e2b1
belard.melanie@gmail.com <belard.melanie@gmail.com>
2020-08-14 16:34:55 +0200

    Masque la clé API

--- open_api.php
+++ open_api.php
@@ -1,9 +1,11 @@
 <?php 
 
+    require("./config.php");
+
 	if (!empty($_GET['lat']) && !empty($_GET['long'])) {
 		$lat = $_GET['lat'];
 		$long = $_GET['long'];
-		$meteoAPIFile = "https://api.darksky.net/forecast/8e3584c81b63a5c4157f4c04d2eda60c/{$lat},{$long}?exclude=minutely-hourly-daily&units=auto&lang=fr";
+		$meteoAPIFile = "https://api.darksky.net/forecast/".API_KEY."/{$lat},{$long}?exclude=minutely-hourly-daily&units=auto&lang=fr";
 		$meteoAPI = fopen($meteoAPIFile,"r");
 		$meteoJson = file_get_contents($meteoAPIFile);
 
